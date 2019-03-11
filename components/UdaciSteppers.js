2948cc4f9e231d401c42b89d04302a89b330c5e3
AlanP <alan.perius.mv@gmail.com>
2019-03-11 20:45:53 -0300

    Udacity studies

---
+++ components/UdaciSteppers.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {Entypo, FontAwesome} from '@expo/vector-icons'
+
+
+export default function UdaciSteppers({max, unit, step, value, onIncrement, onDecrement}) {
+
+
+    return (
+        <View>
+            <View>
+                <TouchableOpacity onPress={onDecrement}>
+                    <FontAwesome
+                        name='minus'
+                        size={30}
+                        color={'black'}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={onIncrement}>
+                    <FontAwesome
+                        name='plus'
+                        size={30}
+                        color={'black'}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <Text> {value}</Text>
+                <Text> {unit} </Text>
+            </View>
+
+        </View>
+
+    );
+}
+
+
---
+++ utils/api.js
@@ -0,0 +1,11 @@
+import {AsyncStorage } from 'react-native'
+import {CALENDAR_STORAGE_KEY} from "./_calendar";
+
+
+export function submitEntry() {
+    
+}
+
+export function submitEntry() {
+
+}
